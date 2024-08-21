a5cd1ca392384fdcc65e6d847d36f94fa7e7d0e3
Rh4096 <rhelvetican@gmail.com>
2024-08-22 01:11:44 +0700

    corelib2 -> transfer_protocol_999_rc2

--- src/odpyx/server/util/__init__.py
+++ src/odpyx/server/util/__init__.py
@@ -1,6 +1,10 @@
+
 from typing import Any
 from .json import JsonUtils, read_json, write_json
 from .crypto import decrypt_battle_data
+
+from os import path as ospath, makedirs
+from requests import get
 
 def update_data(url: str) -> Any:
     BASE_URL_LIST = [
@@ -22,4 +26,19 @@ def update_data(url: str) -> Any:
         ),
     ]
 
-    return 0
+    localPath = ""
+
+    for index in BASE_URL_LIST:
+        if index[0] in url:
+            if not ospath.isdir(index[1]):
+                makedirs(index[1])
+            localPath = url.replace(index[0], index[1])
+            break
+
+    if "Android/version" in url:
+        reponse = get(url)
+        data = JsonUtils.load(reponse.content)
+    else:
+        data = read_json(localPath, encoding="utf-8")
+
+    return data
